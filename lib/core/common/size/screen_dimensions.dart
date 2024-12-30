7b2e8f1b0ba34dcd417a3ef4fde51f06121a583c
A7medMajed16 <ahmedbark129@gmail.com>
2024-12-30 17:14:25 +0200

    refactor: Remove Android TV safety margin logic from screen dimensions calculation

--- lib/core/common/size/screen_dimensions.dart
+++ lib/core/common/size/screen_dimensions.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 
 class ScreenDimensions {
@@ -15,18 +13,5 @@ class ScreenDimensions {
     // Calculate the actual safe area dimensions
     width = mediaQuery.size.width - viewPadding.left - viewPadding.right;
     height = mediaQuery.size.height - viewPadding.top - viewPadding.bottom;
-
-    // For Android TV, you might want to add additional safety margin
-    if (Platform.isAndroid) {
-      // Check if running on Android TV
-      bool isAndroidTV = mediaQuery.size.width / mediaQuery.size.height > 1.5;
-
-      if (isAndroidTV) {
-        // Add safety margin (typically 5-10% of the screen)
-        double safetyMargin = 0.05; // 5%
-        width = width * (1 - safetyMargin * 2);
-        height = height * (1 - safetyMargin * 2);
-      }
-    }
   }
 }
